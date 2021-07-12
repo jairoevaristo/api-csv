5f08ea18c3c7a8428f31077884e32b8a48acce6d
Jairo Gomes <jairoevaristodev@gmail.com>
2021-07-12 15:59:37 -0300

    :construction_worker: create service the product

---
+++ src/dtos/createProduct.ts
@@ -0,0 +1,6 @@
+export interface ICreateProduct {
+  name: string;
+  category: string;
+  quantity: number;
+  price: number;
+}
---
+++ src/service/ProductServices.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository, Repository } from "typeorm";
+
+import { Product } from '../entity/Product';
+import { ProductRepository } from '../repository/ProductRepository';
+import { ICreateProduct } from '../dtos/createProduct';
+
+export class ProductServices {
+  private productRepository: Repository<Product>;
+
+  constructor() {
+    this.productRepository = getCustomRepository(ProductRepository);
+  }
+
+  async execute({ category, name, price, quantity }: ICreateProduct): Promise<Product> {
+    const product = this.productRepository.create({
+      category,
+      name,
+      price,
+      quantity
+    });
+
+    await this.productRepository.save(product);
+    return product;
+  }
+}
